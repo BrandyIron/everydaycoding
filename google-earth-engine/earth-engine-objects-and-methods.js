8ecea8a2c7597a48991e3ddf2e768a914636215f
Brandy Iron <brandy.iron@gmail.com>
2021-12-27 23:32:05 +0900

    20211227

--- google-earth-engine/earth-engine-objects-and-methods.js
+++ google-earth-engine/earth-engine-objects-and-methods.js
@@ -14,4 +14,30 @@ print('e=', serverNumber);
 var logE = serverNumber.log();
 print('log(e)=', logE);
 
+// Make a sequence the hard way.
+var eeList = ee.List([1, 2, 3, 4, 5]);
+// Make a sequence the easy way!
+var sequence = ee.List.sequence(1, 5);
+print('sequence', sequence);
 
+// Use a method on an ee.List to extract a value.
+var value = sequence.get(2);
+print('Value at index 2:', value);
+
+// Cast the return value of get() to a number.
+print('No error:', ee.Number(value).add(3));
+
+// Make a Dictionary on the server.
+var dictionary = ee.Dictionary({
+  e: Math.E,
+  pi: Math.PI,
+  phi: (1 + Math.sqrt(5)) / 2
+});
+
+// Get some values from the dictionary.
+print('Euler:', dictionary.get('e'));
+print('Pi:', dictionary.get('pi'));
+print('Golden ratio:', dictionary.get('phi'));
+
+// Get all the keys:
+print('Keys:', dictionary.keys());
