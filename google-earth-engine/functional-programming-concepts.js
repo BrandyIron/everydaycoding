8b2b371af2dc82b3bfd50185bdfaf921f708892c
Brandy Iron <brandy.iron@gmail.com>
2021-12-28 22:33:35 +0900

    20211228

--- google-earth-engine/functional-programming-concepts.js
+++ google-earth-engine/functional-programming-concepts.js
@@ -11,3 +11,21 @@ var computeSquares = function(number) {
 // Apply your function to each item in the list by using the map() function.
 var squares = myList.map(computeSquares);
 print(squares);
+
+// The following function determines if a number is even or odd. The mod(2)
+// function returns 0 if the number is even and 1 if it is odd (the remainder
+// after dividing by 2). The input is multiplied by this remainder so even
+// numbers get set to 0 and odd numbers are left changed.
+var getOddNumbers = function(number) {
+  number = ee.Number(number); // Cast the input to a Number so we can use mod.
+  var remainder = number.mod(2);
+  return number.multiply(remainder);
+};
+
+var newList = myList.map(getOddNumbers);
+
+// Remove the 0 values.
+var oddNumbers = newList.removeAll([0]);
+
+var squares = oddNumbers.map(computeSquares);
+print(squares);
