4f2764f5718acd5ff1392fce835dd347c66eac7c
Brandy Iron <brandy.iron@gmail.com>
2021-12-30 23:05:07 +0900

    202112302

--- google-earth-engine/visualizing-images-and-image-bands.js
+++ google-earth-engine/visualizing-images-and-image-bands.js
@@ -8,3 +8,7 @@ Map.setCenter(-112.8598, 36.2841, 9); // Center on the Grand Canyon
 Map.addLayer(image);
 
 print('SRTM image', image);
+
+Map.addLayer(image, {min: 0, max: 3000}, 'custom visualization');
+
+Map.addLayer(image, {min: 0, max: 3000, palette: ['blue', 'green', 'red']}, 'custom palette')
