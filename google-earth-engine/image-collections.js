0458617420a7cdcb0b38a74a37dc37b356a710a8
Brandy Iron <brandy.iron@gmail.com>
2022-01-02 17:32:43 +0900

    20220102

--- google-earth-engine/image-collections.js
+++ google-earth-engine/image-collections.js
@@ -10,3 +10,12 @@ var sorted = temporalFiltered.sort('CLOUD_COVER');
 // Get the first (least cloudy) image.
 var scene = sorted.first();
 
+Map.centerObject(scene, 9);
+Map.addLayer(scene, {}, 'default RGB');
+
+var visParams = {bands: ['B4', 'B3', 'B2'], max: 0.5};
+Map.addLayer(scene, visParams, 'true-color composite');
+
+var l8 = ee.ImageCollection('LANDSAT/LC08/C01/T1_TOA');
+var landsat2016 = l8.filterDate('2016-01-01', '2016-12-31');
+Map.addLayer(landsat2016, visParams, 'l8 collection');
