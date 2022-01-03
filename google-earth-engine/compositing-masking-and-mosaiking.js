d062147873f52b4c0106a408cbaa65798a4b9efc
Brandy Iron <brandy.iron@gmail.com>
2022-01-03 21:52:54 +0900

    20220103

---
+++ google-earth-engine/compositing-masking-and-mosaiking.js
@@ -0,0 +1,7 @@
+var visParams = {bands: ['B4', 'B3', 'B2'], max: 0.5};
+
+var l8 = ee.ImageCollection('LANDSAT/LC08/C01/T1_TOA');
+var landsat2016 = l8.filterDate('2016-01-01', '2016-12-31');
+Map.addLayer(landsat2016, visParams, 'l8 collection');
+
+
