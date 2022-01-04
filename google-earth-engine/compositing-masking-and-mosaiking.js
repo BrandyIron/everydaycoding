1d41d579635188a041ce6dac7e126f8e14b4b98c
Brandy Iron <brandy.iron@gmail.com>
2022-01-04 21:21:57 +0900

    20220104

--- google-earth-engine/compositing-masking-and-mosaiking.js
+++ google-earth-engine/compositing-masking-and-mosaiking.js
@@ -1,7 +1,16 @@
+
 var visParams = {bands: ['B4', 'B3', 'B2'], max: 0.5};
 
 var l8 = ee.ImageCollection('LANDSAT/LC08/C01/T1_TOA');
 var landsat2016 = l8.filterDate('2016-01-01', '2016-12-31');
 Map.addLayer(landsat2016, visParams, 'l8 collection');
 
+// Get the median over time, in each band, in each pixel.
+var median = l8.filterDate('2016-01-01', '2016-12-31').median();
+
+// Make a handy variable of visualization parameters.
+var visParams = {bands: ['B4', 'B3', 'B2'], max: 0.3};
+
+// Display the median composite.
+Map.addLayer(median, visParams, 'median');
 
