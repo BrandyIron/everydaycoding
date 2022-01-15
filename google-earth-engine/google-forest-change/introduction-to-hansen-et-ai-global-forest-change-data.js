155a777d68a12f0597d24183f633c0d159ccc491
Brandy Iron <brandy.iron@gmail.com>
2022-01-15 20:51:00 +0900

    2022011501

---
+++ google-earth-engine/google-forest-change/introduction-to-hansen-et-ai-global-forest-change-data.js
@@ -0,0 +1,13 @@
+var gfc2014 = ee.Image('UMD/hansen/global_forest_change_2015');
+Map.addLayer(gfc2014);
+
+Map.addLayer(gfc2014, {bands: ['treecover2000']}, 'treecover2000');
+
+Map.addLayer(gfc2014, {bands: ['last_b50', 'last_b40', 'last_b30']}, 'false_color');
+
+Map.addLayer(gfc2014, {bands: ['loss', 'treecover2000', 'gain']}, 'green');
+
+Map.addLayer(gfc2014, {
+  bands: ['loss', 'treecover2000', 'gain'],
+  max: [1, 255, 1]
+}, 'forest cover, loss, gain');
