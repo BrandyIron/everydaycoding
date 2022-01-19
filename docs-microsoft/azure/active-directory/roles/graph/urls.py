cf4bd62aecc2e04969b9aac9dc9c3a9da28f92aa
Brandy Iron <brandy.iron@gmail.com>
2022-01-19 23:35:00 +0900

    2022011902

---
+++ docs-microsoft/azure/active-directory/roles/graph/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from tutorial import views
+
+urlpatterns = [
+    # /
+    path('', include('tutorial.urls')),
+    path('admin/', admin.site.urls),
+]
