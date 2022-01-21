869338be62870f6af74c4388f738b8ef3e2a136e
Brandy Iron <brandy.iron@gmail.com>
2022-01-21 23:58:36 +0900

    2022012102

--- docs-microsoft/graph/tutorial/views.py
+++ docs-microsoft/graph/tutorial/views.py
@@ -4,5 +4,20 @@ from django.urls import reverse
 from datetime import datetime, timedelta
 
 def home(request):
-    # Temporary!
-    return HttpResponse("Welcome to the tutorial.")
+    context = initialize_context(request)
+
+    return render(request, 'tutorial/home.html', context)
+
+def initialize_context(request):
+    context = {}
+
+    # Check for any errors in the session
+    error = request.session.pop('flash_error', None)
+
+    if error != None:
+        context['errors'] = []
+        context['errors'].append(error)
+    
+    # Check for user in the session
+    context['user'] = request.session.get('user', {'is_authenticated': False})
+    return context
