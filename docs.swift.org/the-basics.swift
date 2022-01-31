c4b42089c7ff98e67fee7a5a8a7def0077561ff4
Brandy Iron <brandy.iron@gmail.com>
2022-01-31 22:47:24 +0900

    2022013103

---
+++ docs.swift.org/the-basics.swift
@@ -0,0 +1,40 @@
+let maximumNumberOfLoginAttempts = 10
+var currentLoginAttempt = 0
+
+var x = 0.0, y = 0.0, z = 0.0
+
+var welcomeMessage: String
+
+welcomeMessage = "Hello"
+
+var red, green, blue: Double
+
+let π = 3.14159
+let 你好 = "你好世界"
+let 🐶🐮 = "dogcow"
+
+var friendlyWelcome = "Hello!"
+friendlyWelcome = "Bonjour!"
+// friendlyWelcome is now "Bonjour!"
+
+let languageName = "Swift"
+languageName = "Swift++"
+// This is a compile-time error: languageName cannot be changed.
+
+print(friendlyWelcome)
+// Prints "Bonjour!"
+
+print("The current value of friendlyWelcome is ¥(friendlyWelcome¥)")
+// Prints "The current value of friendlyWelcome is Bonjour!"
+
+// This is a comment.
+
+/* This is also a comment
+but is written over multiple lines. */
+
+/* This is the start of the first multiline comment.
+    /* This is the second, nested multiline comment. */
+This is the end of the first multiline comment. */
+
+let cat = "🐱"; print(cat)
+// Prints "🐱"
