a1c0a906c822e15f26a36540b3421c3fc626bb4f
Brandy Iron <brandy.iron@gmail.com>
2022-02-01 21:11:22 +0900

    2022020102

--- docs.swift.org/the-basics.swift
+++ docs.swift.org/the-basics.swift
@@ -38,3 +38,46 @@ This is the end of the first multiline comment. */
 
 let cat = "🐱"; print(cat)
 // Prints "🐱"
+
+let minValue = UInt8.min // minValue is equal to 0, and is of type UInt8
+let maxValue = UInt8.max // maxValue is equal to 255, and is of type UInt8
+
+let meaningOfLife = 42
+// meaningOfLife is inferred to be of the type Int
+
+let pi = 3.14159
+// pi is inferred to be of the type Double
+
+let anotherPi = 3 + 0.14159
+// anotherPi is also inferred to be of the Double
+
+let decimalInteger = 17
+let binaryInteger = 0b10001 // 17 in binary notation
+let octalInteger = 0o21 // 17 in octal notation
+let hexadecimalInteger = 0x11 // 17 in hexadecimal notation
+
+let decimalDouble = 12.1875
+let exponentDouble = 1.21875e1
+let hexadecimalDouble = 0xC.3p0
+
+let paddedDouble = 000123.456
+let oneMillion = 1_000_000
+let justOverOneMillion = 1_000_000.000_000_1
+
+let cannotBeNegative: UInt8 = -1
+// UInt8 can't store negative numbers, and so this will report an error
+let tooBig: Int8 = Int8.max + 1
+// Int8 can't store a number larger than its maximum value,
+// and so this will also report an error
+
+let twoThousand: UInt16 = 2_000
+let one: UInt8 = 1
+let twoThousandAndOne = twoThousand + UInt16(one)
+
+let three = 3
+let pointOneFourOneFiveNine = 0.14159
+let pi = Double(three) + pointOneFourOneFiveNine
+// pi equals 3.14159, and is inferred to be of type Double
+
+let integerPi = Int(pi)
+// integerPi equals 3, and is inferred to be of type Int
