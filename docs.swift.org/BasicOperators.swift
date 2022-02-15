b8496318e8c7fbc30e0ada9b6726054640c2cdf4
Brandy Iron <brandy.iron@gmail.com>
2022-02-15 22:20:11 +0900

    20220215

--- docs.swift.org/BasicOperators.swift
+++ docs.swift.org/BasicOperators.swift
@@ -16,3 +16,19 @@ let plusThree = -minusThree // plusThree equals 3, or "minus minus three"
 
 let minusSix = -6
 let alsoMinusSix = +minusSix // alsoMinusSix equals -6
+
+var a = 1
+a += 2
+// a is now equal to 3
+
+let name = "world"
+if name == "world" {
+    print("hello, world")
+} else {
+    print("I'm sorry ¥(name), but I don't recognize you")
+}
+
+(1, "zebra") < (2, "apple") // true because 1 is less than 2; "zebra" and "apple" aren't compared
+(3, "apple") < (3, "bird") // true because 3 is equal to 3, and "apple" is less than "bird"
+(4, "dog") == (4, "dog") // true because 4 is equal to 4, and "dog" is equal to "dog"
+
