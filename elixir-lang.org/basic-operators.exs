efc689b5c99022607a77de5f285d41c29481affb
Brandy Iron <brandy.iron@gmail.com>
2022-02-16 22:24:08 +0900

    20220216

---
+++ elixir-lang.org/basic-operators.exs
@@ -0,0 +1,18 @@
+[1, 2, 3] ++ [4, 5, 6]
+[ 1, 2, 3] -- [2]
+"foo" <> "bar"
+
+true and true
+false or is_atom(:example)
+
+false and raise("This error will never be raised")
+
+1 || true
+false || 11
+
+nil && 13
+true && 17
+
+!true
+!1
+!nil
