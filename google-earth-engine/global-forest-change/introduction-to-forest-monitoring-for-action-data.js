e10e63d2e4880cf47063eaf7007e10cd2325c274
Brandy Iron <brandy.iron@gmail.com>
2022-03-17 23:16:26 +0900

    20120317

--- google-earth-engine/global-forest-change/introduction-to-forest-monitoring-for-action-data.js
+++ google-earth-engine/global-forest-change/introduction-to-forest-monitoring-for-action-data.js
@@ -14,3 +14,38 @@ Map.addLayer(
     {palette: ['FF0000']},
     'FORMA alerts in 2012'
 );
+
+// Load country features from Large Scale International Boundary (LSIB) dataset.
+var countries = ee.FeatureCollection('USDOS/LSIB_SIMPLE/2017');
+
+// Subset the Congo Republic feature from countries.
+var congo = ee.Feature(
+    countries.filter(ee.Filter.eq('country_na', 'Rep of the Congo')).first()
+);
+
+// Subset protected areas to the bounds of the congo feature
+// and other criteria. Clip to the intersection with congo.
+var protectedAreas = ee.FeatureCollection('WCMC/WDPA/current/polygons').filter(ee.Filter.and(
+    ee.Filter.bounds(congo.geometry()),
+    ee.Filter.neq('IUCN_CAT', 'VI'),
+    ee.Filter.neq('STATUS', 'proposed'),
+    ee.Filter.lt('STATUS_YR', 2010)
+)).map(function(fast){
+    return congo.intersection(feat);
+});
+
+// Display protected areas on the map.
+Map.addLayer(
+    protectedAreas,
+    {color: '000000'},
+    'Congo Republic protected areas'
+);
+
+// Calculate the number of FORMA pixels in protected
+// areas of the Congo Republic, 2012.
+var stats = forma2012.reduceRegion({
+    reducer: ee.Reducer.sum(),
+    geometry: protectedAreas.geometry(),
+    scale: 500
+});
+print('Number of FORMA pixels, 2012: ', stats.get('constant'));
