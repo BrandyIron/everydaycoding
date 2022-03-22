06699a896180511aeb3992d5cd60da219cfc9342
Brandy Iron <brandy.iron@gmail.com>
2022-03-22 22:39:55 +0900

    20220322

--- google-earth-engine/global-surface-water/water-occurrence-(1984-2015).js
+++ google-earth-engine/global-surface-water/water-occurrence-(1984-2015).js
@@ -3,3 +3,15 @@ var occurrence = gsw.select('occurance');
 Map.addLayer(occurrence);
 
 Map.addLayer((eeObject: occurrence, name: 'Water Occurrence (1984-2015)' ));
+
+var VIS_OCCURRENCE = {
+    min: 0,
+    max: 100,
+    palette: ['red', 'blue']
+};
+
+Map.addLayer({
+    eeObject: occurrence.updateMask(occurrence.divide(100)),
+    name: 'Water Occurrence (1984-2015',
+    visParams: VIS_OCCURRENCE
+});
