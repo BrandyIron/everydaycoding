51bf9f8720d579d6424e4d41398b87d117adbddb
Brandy Iron <brandy.iron@gmail.com>
2022-03-26 20:49:40 +0900

    20220326

--- google-earth-engine/global-surface-water/water-occurrence-(1984-2015).js
+++ google-earth-engine/global-surface-water/water-occurrence-(1984-2015).js
@@ -45,3 +45,23 @@ var VIS_OCCURRENCE = {
 var VIS_WATER_MASK = {
     palette: ['white', 'black']
 };
+
+// Calculations
+// Create a water mask layer, and set the image mask so that non-water areas are opaque
+var water_mask = occurrence.gt(90).unmask(0);
+
+// Initialize Map Location
+Map.setCenter(-74.4557, -8.4289, 11); // Ucayali River, Peru
+
+// Map Layers
+Map.addLayer({
+    eeObject: water_mask,
+    visParams: VIS_WATER_MASK,
+    name: '90% occurrence water mask',
+    shown: false
+});
+Map.addLayer({
+    eeObject: occurrence.updateMask(occurrence.divide(100)),
+    name: 'Water Occurrence (1984-2015)',
+    visParams: VIS_OCCURRENCE
+});
