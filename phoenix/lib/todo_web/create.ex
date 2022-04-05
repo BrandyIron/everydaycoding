b807a2e43a5e5a25ed15601c7ae6fad0f3e8f26c
Brandy Iron <brandy.iron@gmail.com>
2022-04-05 23:23:58 +0900

    20220405

--- phoenix/lib/todo_web/create.ex
+++ phoenix/lib/todo_web/create.ex
@@ -4,3 +4,8 @@ scope "/", TodoWeb do
   get "/items", ItemsController, :index
   post "/items", ItemsController, :create
 end
+
+def create(conn, %{"item" => item_params}) do
+  Items.create_item(item_params)
+  redirect(conn, to: "/items")
+end
