8ba953320154cb86c0105dd68d55c5a344e30a5f
Brandy Iron <brandy.iron@gmail.com>
2022-04-07 23:16:58 +0900

    20220407

--- phoenix/lib/todo_web/controllers/item_controller.ex
+++ phoenix/lib/todo_web/controllers/item_controller.ex
@@ -8,3 +8,20 @@ defmodule TodoWeb.ItemController do
     render(conn, "index.html", items: items)
   end
 end
+
+defmodule TodoWeb.ItemsController do
+  use TodoWeb, :controller
+
+  alias Todo.Items
+  alias Todo.Items.Item
+
+  def index(conn, _params) do
+    items = Items.list_items()
+    changeset = Item.changeset(%Item{}, %{})
+    render(conn, "index.html", items: items, changeset: changeset)
+  end
+
+  def create(conn, %{"item" => item_params}) do
+    Items.create_item(item_params)
+    redirect(conn, to: "/items")
+  end
