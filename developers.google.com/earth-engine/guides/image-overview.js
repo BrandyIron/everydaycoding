5a684973721ae5062c3edb29af8960ef7b4b0670
Brandy Iron <brandy.iron@gmail.com>
2022-04-28 22:39:05 +0900

    20220428

--- developers.google.com/earth-engine/guides/image-overview.js
+++ developers.google.com/earth-engine/guides/image-overview.js
@@ -3,3 +3,10 @@ var loadedImage = ee.Image('JAXA/ALOS/AW3D30/V2_2');
 var first = ee.ImageCollection('COPERNICS/S2_SR').filterBounds(ee.Geometry.Point(-70.48, 43.3631)).firstDate('2019-01-01', '2019-12-31').sort('CLOUDY_PIXEL_PERCENTAGE').first();
 Map.centerObject(first, 11);
 Map.addLayer(first, {bands: ['B4', 'B3', 'B2'], min: 0, max: 2000}, 'first');
+
+var uri = 'gs://gcp-public-data-landsat/LC08/01/001/002/' +
+'LC08_L1GT_001002_20160817_20170322_01_T2/' +
+'LC08_L1GT_001002_20160817_20170322_01_T2_B5.TIF';
+
+var cloudImage = ee.Image.loadGeoTIFF(uri);
+print(cloudImage);
