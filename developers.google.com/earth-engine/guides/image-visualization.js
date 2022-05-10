1954f400379e12d55e35a69f1326d479a6fbdb41
Brandy Iron <brandy.iron@gmail.com>
2022-05-10 21:24:22 +0900

    20220510

--- developers.google.com/earth-engine/guides/image-visualization.js
+++ developers.google.com/earth-engine/guides/image-visualization.js
@@ -20,3 +20,7 @@ var image = ee.Image('LANDSAT/LC08/C02/T1_TOA/LC08_044034_20140318');
 var ndwi = image.normalizeDifference(['B3', 'B5']);
 var ndwiViz = {min: 0.5, max: 1, palette: ['00FFFF', '0000FF']};
 Map.addLayer(ndwi, ndwiViz, 'NDWI', false);
+
+// Mask the non-watery parts of the image, where NDWI < 0.4.
+var ndwiMasked = ndwi.updateMask(ndwi.gte(0.4));
+Map.addLayer(ndwiMasked, ndwiViz, 'NDWI masked');
